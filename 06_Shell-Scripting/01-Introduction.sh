9ee9e19b5069fbcd1669df839781b3c4e7615a8e
Skywalker <70905670+skywalker1319@users.noreply.github.com>
2021-09-23 22:55:13 +0530

    Bash

---
+++ 06_Shell-Scripting/01-Introduction.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author:  Master Skywalker
+# Purpose:  Working with variables...
+# Stardate: 12021.266.09:41:00
+
+
+name='Master Skywalker'
+skills='AI & Robotics system and programming'
+echo
+echo "Name:   $name"
+echo
+echo "Skills:  $skills"
+
+echo
+
+
+
+
---
+++ 06_Shell-Scripting/02-Introduction.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author:  Master Skywalker
+# Purpose:  Working with Input & Output...
+# Stardate: 12021.266.09:41:00
+
+
+echo
+# echo / read
+echo
+
+
+
+
+
